915ed903e4dd9cf409c221d1accf574004ede0d2
Andre <200411100210@student.trunojoyo.ac.id>
2024-01-08 15:36:25 +0700

    First Commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/andreandriand/app-go-module
+
+go 1.21.1
+
+require github.com/andreandriand/go-modules/v2 v2.0.0 // indirect
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	go_modules "github.com/andreandriand/go-modules/v2"
+)
+
+func main() {
+	fmt.Println(go_modules.Hello("andre"))
+}
